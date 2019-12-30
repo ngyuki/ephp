62d1d1b60074a4ef9a50b1f063b9ad5300915cdc
ngyuki <ngyuki.jp@gmail.com>
2019-12-30 22:35:23 +0900

    Support require/include on CachedCompiler

--- src/CachedCompiler.php
+++ src/CachedCompiler.php
@@ -6,26 +6,60 @@ use RuntimeException;
 class CachedCompiler
 {
     /**
-     * @var Compiler
+     * @var string
      */
-    private $compiler;
+    private $sourceDir;
+    /**
+     * @var string
+     */
+    private $compiledDir;
+
+    /**
+     * @var string
+     */
+    private $echo;
 
     /**
      * @var bool
      */
     private $forceCompile;
 
-    public function __construct(string $echo, $forceCompile = false)
+    public function __construct(string $sourceDir, string $compiledDir, string $echo, bool $forceCompile = false)
     {
-        $this->compiler = new Compiler($echo);
+        $this->sourceDir = realpath($sourceDir);
+        if ($this->sourceDir === false) {
+            throw new RuntimeException("Unable realpath \"$sourceDir\"");
+        }
+        $this->compiledDir = realpath($compiledDir);
+        if ($this->compiledDir === false) {
+            throw new RuntimeException("Unable realpath \"$compiledDir\"");
+        }
+        $this->sourceDir = rtrim($this->sourceDir, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
+        $this->compiledDir = rtrim($this->compiledDir, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
+        $this->echo = $echo;
         $this->forceCompile = $forceCompile;
     }
 
-    public function compile(string $sourceFile, string $compiledFile)
+    public function compile(string $sourceFile)
     {
+        $sourceFile = realpath($sourceFile);
+        if ($sourceFile === false) {
+            throw new RuntimeException("Unable realpath \"$sourceFile\"");
+        }
+        if (substr($sourceFile, 0, strlen($this->sourceDir)) !== $this->sourceDir) {
+            return $sourceFile;
+        }
+        $filename = substr($sourceFile, strlen($this->sourceDir));
+        $compiledFile = $this->compiledDir . $filename;
+
         if (!$this->forceCompile && file_exists($compiledFile) && (filemtime($sourceFile) <= filemtime($compiledFile))) {
             return $compiledFile;
         }
+
+        $compiler = new Compiler($this->echo, function (string $expr) {
+            $serialize = var_export(serialize($this), true);
+            return '(unserialize(' . $serialize . '))->compile(' . $expr . ')';
+        });
 
         $compiledDir = dirname($compiledFile);
         if (!is_dir($compiledDir)) {
@@ -33,7 +67,7 @@ class CachedCompiler
         }
 
         $source = file_get_contents($sourceFile);
-        $compiled = $this->compiler->compile($source, $sourceFile);
+        $compiled = $compiler->compile($source, $sourceFile);
         file_put_contents($compiledFile, $compiled);
         return $compiledFile;
     }
--- tests/CachedCompilerTest.php
+++ tests/CachedCompilerTest.php
@@ -8,28 +8,45 @@ class CachedCompilerTest extends TestCase
 {
     function test()
     {
-        $file = __DIR__ . '/_files/01.phtml';
-        $cache = __DIR__ . '/_files/01.phtml.cache';
+        $compiled = (new CachedCompiler(__DIR__ . '/_files/', __DIR__ . '/_files/cache', 'htmlspecialchars', true))
+            ->compile(__DIR__ . '/_files/01.phtml');
 
+        ob_start();
         try {
-            $compiled = (new CachedCompiler('htmlspecialchars'))->compile($file, $cache);
-
-            ob_start();
-            try {
-                require $compiled;
-            } finally {
-                $output = ob_get_clean();
-            }
-            $output = strtr($output, [DIRECTORY_SEPARATOR => '/']);
-            $expected = implode("\n", [
-                '<s>&lt;br&gt;</s>',
-                '<s><br></s>',
-                '<s>%s/tests/_files</s>',
-                '<s>%s/tests/_files/01.phtml</s>',
-            ]);
-            $this->assertStringMatchesFormat($expected, $output);
+            require $compiled;
         } finally {
-            unlink($cache);
+            $output = ob_get_clean();
         }
+        $output = strtr($output, [DIRECTORY_SEPARATOR => '/']);
+        $expected = implode("\n", [
+            '<s>&lt;br&gt;</s>',
+            '<s><br></s>',
+            '<s>%s/tests/_files</s>',
+            '<s>%s/tests/_files/01.phtml</s>',
+        ]);
+        $this->assertStringMatchesFormat($expected, $output);
+    }
+
+    /**
+     * @test
+     */
+    public function inc()
+    {
+        $compiled = (new CachedCompiler(__DIR__ . '/_files/', __DIR__ . '/_files/cache', 'htmlspecialchars', true))
+            ->compile(__DIR__ . '/_files/inc1.phtml');
+
+        ob_start();
+        try {
+            require $compiled;
+        } finally {
+            $output = ob_get_clean();
+        }
+        $output = strtr($output, [DIRECTORY_SEPARATOR => '/']);
+        $expected = implode("\n", [
+            'this is &amp;inc1.phtml',
+            'this is &amp;inc2.phtml',
+            'this is &amp;inc3.phtml',
+        ]);
+        $this->assertStringMatchesFormat($expected, $output);
     }
 }
